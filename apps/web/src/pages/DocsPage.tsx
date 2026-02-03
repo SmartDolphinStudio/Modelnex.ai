4f0f1bea779288716a28e2a07211ad20db0a5b04
SmartDolphinStudio <SmartDolphinStudio@gmail.com>
2026-02-03 12:04:54 +0800

    feat: add pages docspage

---
+++ apps/web/src/pages/DocsPage.tsx
@@ -0,0 +1,9 @@
+import DocsWorkspace from '../features/docs/main'
+
+export default function DocsPage() {
+  return (
+    <div className="modelnex-docs" aria-label="ModelNex documentation">
+      <DocsWorkspace />
+    </div>
+  )
+}
